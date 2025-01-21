59174e27f1e374bae314a8a2bbadd0998fe36eb9
Jim O'Brien <jim.obrien3@gmail.com>
2025-01-21 00:37:29 -0500

    adding github actions

--- next.config.js
+++ next.config.js
@@ -7,5 +7,7 @@ module.exports = {
   images: {
     domains: ['media.dev.to', 'avatars.githubusercontent.com', 'github-readme-stats.vercel.app', 'github-readme-streak-stats.herokuapp.com', 'stardev.io', 'github-profile-summary-cards.vercel.app', 'github-profile-trophy.vercel.app'],
     dangerouslyAllowSVG: true,
-  }
+  },
+  output: "export",
+  reactStrictMode: true,
 }
